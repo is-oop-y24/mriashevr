9e496b5d747d3952bb993dd7a8082793c31a8659
Mariia <shevrinam@mail.ru>
2021-09-15 17:11:08 +0300

    Initial Commit

--- Isu.Tests/IsuServiceTest.cs
+++ Isu.Tests/IsuServiceTest.cs
@@ -1,3 +1,4 @@
+using Isu.Entities;
 using Isu.Services;
 using Isu.Tools;
 using NUnit.Framework;
@@ -11,22 +12,27 @@ namespace Isu.Tests
         [SetUp]
         public void Setup()
         {
-            //TODO: implement
-            _isuService = null;
+            _isuService = new IsuService();
         }
 
         [Test]
         public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
         {
-            Assert.Fail();
+            Group group =_isuService.AddGroup("M3208");
+            Student student = _isuService.AddStudent(group, "blya");
+            Assert.Contains(student, group.Students);
         }
 
         [Test]
         public void ReachMaxStudentPerGroup_ThrowException()
         {
+            Group group = _isuService.AddGroup("M3211");
             Assert.Catch<IsuException>(() =>
             {
-                
+                for (int i = 0; i <= 31; i++)
+                {
+                    Student student = _isuService.AddStudent(group, "A");
+                }
             });
         }
 
@@ -35,17 +41,19 @@ namespace Isu.Tests
         {
             Assert.Catch<IsuException>(() =>
             {
-
+                _isuService.AddGroup("Test");
             });
         }
 
         [Test]
         public void TransferStudentToAnotherGroup_GroupChanged()
         {
-            Assert.Catch<IsuException>(() =>
-            {
-
-            });
+            Group group1 = _isuService.AddGroup("M3211");
+            Group group2 = _isuService.AddGroup("M3212");
+            Student student = _isuService.AddStudent(group1, "Ivan");
+            _isuService.ChangeStudentGroup(student, group2);
+            Assert.Contains(student, group2.Students);
+            Assert.True(group1.Students.Count == 0);
         }
     }
 }
--- Isu/Program.cs
+++ Isu/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace Isu
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using Isu.Entities;
+using Isu.Services;
+
+namespace Isu
 {
     internal class Program
     {
         private static void Main()
         {
+            var isuService = new IsuService();
+
+            // var group = isuService.AddGroup("M3203");
+            // Console.WriteLine("all good");
+            //
+            // isuService.AddStudent(group, "fuck");
+            // isuService.AddStudent(group, "suck");
+            // Console.WriteLine("Sonya and Masha are besties forever!!!");
+            //
+            // Console.WriteLine(isuService.GetStudent(100001).Name);
+            //
+            // List<Student> students = new List<Student>();
+            // students = isuService.FindStudents("M3203");
+            //
+            // foreach (var student in students)
+            // {
+            //     Console.WriteLine(student.Name);
+            // }
+            //
+            // List<Student> courseStudents = new List<Student>();
+            // CourseNumber course = new CourseNumber(2);
+            // courseStudents = isuService.FindStudents(course);
+            //
+            // foreach (Student student in courseStudents)
+            //  {
+            //      Console.WriteLine(student.Name);
+            //  }
+
+            // Console.WriteLine(isuService.FindStudents("M3203"));
+            var group1 = isuService.AddGroup("M3203");
+            var student = isuService.AddStudent(group1, "ivan");
+            var group2 = isuService.AddGroup("M3202");
+            isuService.ChangeStudentGroup(student, group2);
+            Console.WriteLine("it is student group1");
+            foreach (var stud in group1.Students)
+            {
+                Console.WriteLine(stud.Name);
+            }
+
+            Console.WriteLine("it is student group2");
+            foreach (var stud in group2.Students)
+            {
+                Console.WriteLine(stud.Name);
+            }
         }
     }
 }

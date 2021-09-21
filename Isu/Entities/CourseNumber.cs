8afe86b655c13e37748f2476306bc6a605a8497f
Mariia <shevrinam@mail.ru>
2021-09-21 15:49:28 +0300

    Initial commit

---
+++ Isu/Entities/CourseNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Isu.Entities
+{
+    public class CourseNumber
+    {
+        public CourseNumber(int course)
+        {
+            Course = course;
+        }
+
+        public int Course { get; }
+    }
+
+    // public enum CourseNumber
+    // {
+    //     Fisrt = 1,
+    //     Second = 2,
+    //     Third = 3,
+    //     Fourth = 4,
+    // }
+}

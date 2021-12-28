5150d8e50d7e7f637281e0b9d342989375e6e332
Mariia <shevrinam@mail.ru>
2021-12-28 20:05:28 +0300

    initial commit lab-5

---
+++ BackupsExtra/Entities/JsonConvert.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BackupsExtra
+{
+    public class JsonConvert
+    {
+        public JsonConvert()
+        {
+        }
+
+        // string _json = JsonConvert.
+    }
+}
---
+++ BackupsExtra/Entities/Merge.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Backups.Entities;
+
+namespace BackupsExtra
+{
+    public class Merge
+    {
+        public List<RestorePoint> MergeToLast(List<RestorePoint> restorePoints)
+        {
+            RestorePoint restorePoint = restorePoints.Last();
+            foreach (RestorePoint point in restorePoints)
+            {
+                foreach (Storage storage in point.Storages)
+                {
+                    if (!restorePoint.Storages.Contains(storage))
+                    {
+                        restorePoint.Storages.Add(storage);
+                    }
+                }
+            }
+
+            return restorePoints;
+        }
+
+        public List<RestorePoint> DeleteRestorePoint(List<RestorePoint> restorePoints)
+        {
+            RestorePoint restorePoint = restorePoints.Last();
+            foreach (RestorePoint point in restorePoints)
+            {
+                foreach (Storage storage in point.Storages)
+                {
+                    if (restorePoint.Storages.Contains(storage))
+                    {
+                        point.Storages.Remove(storage);
+                    }
+                }
+            }
+
+            return restorePoints;
+        }
+
+        public List<RestorePoint> DeleteSingleStored(List<RestorePoint> restorePoints)
+        {
+            RestorePoint restorePoint = restorePoints.Last();
+            foreach (RestorePoint point in restorePoints)
+            {
+                if (point.Storages.Count == 0 && point != restorePoint)
+                {
+                    restorePoints.Remove(point);
+                }
+            }
+
+            return restorePoints;
+        }
+    }
+}

8420cc4694b0d580d1ee9edc9ed4a919c4bfeaf0
Mariia <shevrinam@mail.ru>
2021-12-28 20:10:45 +0300

    refactor:resolving conflict

--- Isu/Services/IIsuService.cs
+++ Isu/Services/IIsuService.cs
@@ -1,9 +1,13 @@
-﻿namespace Isu.Services
+﻿using System.Collections.Generic;
+using Isu.Entities;
+
+namespace Isu.Services
 {
     public interface IIsuService
     {
         Group AddGroup(string name);
         Student AddStudent(Group group, string name);
+
         Student GetStudent(int id);
         Student FindStudent(string name);
         List<Student> FindStudents(string groupName);
